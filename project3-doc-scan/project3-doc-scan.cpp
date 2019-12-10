e88dbd4f3894bd10f90b433bce4f51d10981f8ee
leonyu1010 <11080856+leonyu1010@users.noreply.github.com>
2019-12-10 13:32:19 +0800

    update doc scanner

--- project3-doc-scan/project3-doc-scan.cpp
+++ project3-doc-scan/project3-doc-scan.cpp
@@ -2,10 +2,14 @@
 //
 
 #include <iostream>
+#include <vector>
+
 #include "opencv2/opencv.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
+
+using namespace std;
 using namespace cv;
 
 // On every area selection, changes are updated through binmask
@@ -22,26 +26,84 @@ int main(int argc, char **argv)
 {
 	Mat image, mask, bgdModel, fgdModel, binMask, grabcutImage;
 
-	Mat originalImage = imread("./data/images/doc.jpg");
+	Mat originalImage = imread("./data/images/book1.jpg");
 	image = originalImage.clone();
 
 	mask.create(image.size(), CV_8UC1);
-	Rect rect(10, 10, originalImage.cols - 20, originalImage.rows - 20);
+	int topmargin = 250;
+	int bottommargin = 80;
+	int leftmargin = 60;
+	int rightmargin = 60;
+	Rect rect(leftmargin, topmargin, originalImage.cols - (leftmargin + rightmargin),
+			  originalImage.rows - (topmargin + bottommargin));
 	mask.setTo(GC_BGD);
 	(mask(rect)).setTo(Scalar(GC_PR_FGD));
 
 	grabCut(image, mask, rect, bgdModel, fgdModel, 1, GC_INIT_WITH_RECT);
 
-	for (int i = 0; i < 5; i++)
-	{
-		grabCut(image, mask, rect, bgdModel, fgdModel, 1);
-	}
+	// for (int i = 0; i < 5; i++)
+	//{
+	//	grabCut(image, mask, rect, bgdModel, fgdModel, 1);
+	//}
 	getBinMask(mask, binMask);
 	image.copyTo(grabcutImage, binMask);
 
-	imshow("original", originalImage);
-	waitKey(1);
+	// imshow("original", originalImage);
+	// waitKey(1);
 	imshow("grabcut", grabcutImage);
-	waitKey(0);
+	waitKey(1);
+
+	Mat gray;
+	cvtColor(grabcutImage, gray, COLOR_BGR2GRAY);
+
+	Mat binary;
+	threshold(gray, binary, 10, 255, THRESH_BINARY);
+
+	Mat eroded;
+	erode(binary, eroded, getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
+	imshow("eroded", eroded);
+	waitKey(1);
+
+	vector<Point> contours;
+	vector<vector<Point>> contours0;
+	vector<Vec4i> hierarchy;
+
+	findContours(eroded, contours0, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+	double maxArea = -1;
+	double area = -1;
+	int contourId = -1;
+	for (size_t i = 0; i < contours0.size(); i++)
+	{
+		area = contourArea(contours0[i]);
+		if (area > maxArea)
+		{
+			maxArea = area;
+			contourId = i;
+		}
+		cout << "Contour #" << i + 1 << " area = " << area << " perimeter = " << arcLength(contours0[i], true) << endl;
+	}
+
+	approxPolyDP(Mat(contours0[contourId]), contours, 50, true);
+
+	Mat rotatedBox = originalImage.clone();
+	RotatedRect rotrect;
+	Point2f rect_points[4];
+	Mat boxPoints2f, boxPointsCov;
+	// Rotated rectangle
+	rotrect = minAreaRect(contours);
+	boxPoints(rotrect, boxPoints2f);
+	boxPoints2f.assignTo(boxPointsCov, CV_32S);
+	polylines(rotatedBox, boxPointsCov, true, Scalar(0, 255, 255), 2);
+	imshow("rotatedBox", rotatedBox);
+	waitKey(1);
+	Mat box = originalImage.clone();
+
+	vector<vector<Point>> finalContours;
+	finalContours.push_back(contours);
+	drawContours(box, finalContours, 0, Scalar(128, 255, 255), 3, LINE_AA, hierarchy, 0);
+	imshow("box", box);
+
+	waitKey();
 	return 0;
 }
