6f420e1c76d7ec1b1a9ee6fa27f9d8f9eb20d947
leonyu1010 <11080856+leonyu1010@users.noreply.github.com>
2019-12-13 11:23:30 +0800

    update

--- python/project3-doc-scan/submission.py
+++ python/project3-doc-scan/submission.py
@@ -1,5 +1,6 @@
 import cv2
-import numpy as np;
+import numpy as np
+import math
 
 class DocScanner:
     def __init__(self, file):
@@ -13,7 +14,7 @@ class DocScanner:
         width, height, dst_corners = self.setup_dst_corners()
         output = self.warp_corners(doc_contour, dst_corners, width, height)
 
-        cv2.imshow("input", self.image)
+
         cv2.imshow("output", output)
         cv2.waitKey(0)
 
@@ -25,7 +26,7 @@ class DocScanner:
         dst_bottom_left = (0,dst_height)
         dst_bottom_right = (dst_width, dst_height)
 
-        dst_corners = np.array([dst_top_right, dst_top_left,dst_bottom_left,dst_bottom_right], dtype=int)
+        dst_corners = np.array([dst_top_left, dst_bottom_left, dst_bottom_right, dst_top_right], dtype=int)
         return (dst_width, dst_height, dst_corners)
 
     def detect_edges(self, image):
@@ -53,18 +54,35 @@ class DocScanner:
                 maxArea = area
                 contourId = id
         
-        doc_contour = cv2.approxPolyDP(contours0[contourId], 100, True)   
+        doc_contour = cv2.approxPolyDP(contours0[contourId], 0.05*cv2.arcLength(contours0[contourId],True), True)   
         print(doc_contour)
 
         contours = [doc_contour]
         cv2.drawContours(self.image, contours, 0, (0,0,255), 3, cv2.LINE_AA, hierarchy, 0)
+        cv2.imshow("input", self.image)
+        cv2.waitKey(0)
         return doc_contour
 
+    def calDistance(self, pt1, pt2):
+        distance = math.sqrt(math.pow((pt1[0]-pt2[0]), 2) + math.pow((pt1[1]-pt2[1]), 2))
+        return distance
+
     def warp_corners(self, src_corners, dst_corners, dst_width, dst_height):    
-        h, status = cv2.findHomography(src_corners, dst_corners, cv2.RANSAC, 3)
+        pts = []
+        for pt in src_corners:
+            pts.append((pt[0][0], pt[0][1]))
+
+        distance_12 = self.calDistance(pts[0], pts[1])
+        distance_23 = self.calDistance(pts[1], pts[2]) 
+
+        if distance_12 < distance_23 :
+            h, status = cv2.findHomography(np.array([pts[1], pts[2], pts[3], pts[0]]), dst_corners, cv2.RANSAC, 3)
+        else:
+            h, status = cv2.findHomography(np.array([pts[0], pts[1], pts[2], pts[3]]), dst_corners, cv2.RANSAC, 3)
+
         self.final_image = cv2.warpPerspective(self.image, h, (dst_width, dst_height))
         return self.final_image
-
+ 
 
 if __name__ == '__main__':
     import sys
@@ -72,7 +90,7 @@ if __name__ == '__main__':
         file = sys.argv[1]
     except:
         file = "book3.jpg"
-    for file in ["book1.jpg", "book2.jpg", "book3.jpg","book4.jpg"]:
+    for file in ["book1.jpg", "book3.jpg", "book2.jpg", "book4.jpg"]:
         try:
             DocScanner(file).run()
             cv2.destroyAllWindows()
